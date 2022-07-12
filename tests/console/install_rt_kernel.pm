9df051239bfb00880920afb751b9123319545dbe
pcervinka <pcervinka@suse.com>
2022-07-12 16:23:11 +0200

    Add reboot after kernel-rt installation

--- tests/console/install_rt_kernel.pm
+++ tests/console/install_rt_kernel.pm
@@ -11,14 +11,21 @@ use strict;
 use warnings;
 use testapi;
 use utils qw(zypper_call);
+use power_action_utils 'power_action';
 
 sub run {
     my $self = shift;
     $self->select_serial_terminal;
 
     zypper_call('rm kernel-default');
-    zypper_call('in kernel-rt');
+    zypper_call('in kernel-rt cpupower');
+    script_run('cpupower idle-info');
+    power_action('reboot', textmode => 1);
+    $self->wait_boot;
+    $self->select_serial_terminal;
+    script_run('cpupower idle-info');
 }
+
 
 sub test_flags {
     return {fatal => 1};
