e0c159bcad5432c157527ebd76c19eb2f6b2c004
pcervinka <pcervinka@suse.com>
2023-03-01 13:31:33 +0100

    Update SLERT for 15-SP5

--- tests/kernel/install_ltp.pm
+++ tests/kernel/install_ltp.pm
@@ -315,6 +315,9 @@ sub run {
     select_serial_terminal;
     export_ltp_env;
 
+    script_run "echo nameserver 10.168.192.1 > /etc/resolv.conf";
+    script_run "cat /etc/resolv.conf";
+
     if (script_output('cat /sys/module/printk/parameters/time') eq 'N') {
         script_run('echo 1 > /sys/module/printk/parameters/time');
         $grub_param .= ' printk.time=1';
