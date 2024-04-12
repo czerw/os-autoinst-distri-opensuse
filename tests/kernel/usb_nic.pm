bd1d2f76b41b83fa12834db4eaad3f1d58d1ff07
Petr Cervinka <pcervinka@suse.com>
2024-04-12 19:43:19 +0200

    Update

--- tests/kernel/usb_nic.pm
+++ tests/kernel/usb_nic.pm
@@ -5,7 +5,7 @@
 
 # Package: usb_nic
 # Summary: Simple smoke test for testing USB NIC connected to system
-# Maintainer: LSG QE Kernel <kerneli-qa@suse.de>
+# Maintainer: LSG QE Kernel <kernel-qa@suse.de>
 
 use base 'opensusebasetest';
 use strict;
@@ -13,29 +13,22 @@ use warnings;
 use testapi;
 use serial_terminal 'select_serial_terminal';
 use utils;
-use Utils::Logging 'export_logs_basic';
 
 sub run {
     my ($self) = @_;
 
     select_serial_terminal;
 
-    zypper_call('in -t package ethtool');
-
     my $interface = script_output 'basename $(readlink /sys/class/net/* | grep usb )';
 
     assert_script_run "echo \"BOOTPROTO='dhcp'\" > /etc/sysconfig/network/ifcfg-$interface";
     assert_script_run "echo \"STARTMODE='auto'\" >> /etc/sysconfig/network/ifcfg-$interface";
 
-    assert_script_run "ifup $interface";
+    assert_script_run ("ifup $interface -o debug", 60);
 
-    # wait until interface is up
-    my $timeout = 20;
-    ($timeout-- && sleep 5) while (script_run "ip -4 addr show $interface | grep inet" && $timeout);
-
-    my $ping_peer = script_output "ip route show dev $interface | cut -d ' ' -f 7";
-    assert_script_run "ping -I $interface -c 4 $ping_peer";
-    assert_script_run "ifdown $interface";
+    my $route = script_output "ip route show default";
+    my @ping_peer = $route =~ /default via (\S+)/;
+    assert_script_run "ping -I $interface -c 4 @ping_peer";
 }
 
 sub test_flags {
