c849314a3cdf5bb5954cb883c4601cfa62bf8246
Petr Cervinka <pcervinka@suse.com>
2026-03-25 17:13:57 +0100

    install image: Add UEFI boot entry registration
    Added UEFI boot entry registration after installing an image using dd
    on UEFI-enabled systems, ensuring the system can boot after
    installation.

--- tests/microos/install_image.pm
+++ tests/microos/install_image.pm
@@ -103,6 +103,12 @@ sub run {
     my $final_disk_layout = script_output("parted ${device} --script print");
     record_info("INFO", "${image} was installed on ${device}. System is going to be rebooted.\n\nFinal disk layout:\n ${final_disk_layout}");
 
+    # Register UEFI boot entry after dd
+    if (get_var('IPXE_UEFI')) {
+        my $distri = get_var('DISTRI');
+        assert_script_run("efibootmgr --create --disk ${device} --part 2 --label '${distri}' --loader '\\EFI\\BOOT\\bootx64.efi'");
+    }
+
     # We have to use force option to reboot command as installer doesn't have fully running systemd environment
     power_action("reboot", textmode => 1, force => 1);
 
